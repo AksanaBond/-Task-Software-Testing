0a0de783b903e712a2ac1e4605c7c62db0ecc663
Aksana Bandarenka <oksanakyxnove@icloud.com>
2026-06-08 15:57:30 +0200

    chore: setup ESLint, Prettier, Husky

---
+++ eslint.config.mjs
@@ -0,0 +1,30 @@
+import wdioPlugin from 'eslint-plugin-wdio';
+import prettierPlugin from 'eslint-plugin-prettier/recommended';
+
+export default [
+  prettierPlugin,
+  {
+    plugins: {
+      wdio: wdioPlugin,
+    },
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        describe: 'readonly',
+        it: 'readonly',
+        before: 'readonly',
+        after: 'readonly',
+        beforeEach: 'readonly',
+        afterEach: 'readonly',
+        browser: 'readonly',
+        $: 'readonly',
+        $$: 'readonly',
+        expect: 'readonly',
+      },
+    },
+    rules: {
+      'prettier/prettier': 'error',
+    },
+  },
+];
