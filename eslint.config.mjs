0d9411a7aecec11be7ffc7b3a8c1b56ff7232bf8
Aksana Bandarenka <oksanakyxnove@icloud.com>
2026-07-16 14:51:43 +0200

    feat: add spec and HTML reports

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,15 +2,15 @@ import playwright from 'eslint-plugin-playwright';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 
 export default [
+  {
+    ignores: ['my-report/', 'test-results/'],
+  },
   playwright.configs['flat/recommended'],
 
   eslintPluginPrettierRecommended,
 
   {
     files: ['tests/**/*.spec.js', 'tests/**/*.test.js'],
-    rules: {
-      // Здесь можно добавлять свои правила
-      // 'playwright/expect-expect': 'warn',
-    },
+    rules: {},
   },
 ];
